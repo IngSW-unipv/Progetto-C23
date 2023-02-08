acff9e29a381fac878a8ba0434a35456bf7efaf9
Rebecca Maccagni <119804491+RebeccaMaccagni@users.noreply.github.com>
2023-02-08 19:28:42 +0100

    Revert "Proseguimento"
    This reverts commit 354dce37a197e5135065871ab2b8e55b9ac3faef.

--- FitUP/src/it/unipv/sfw/esame/progettoC23/Persona.java
+++ FitUP/src/it/unipv/sfw/esame/progettoC23/Persona.java
@@ -5,20 +5,18 @@ import java.text.DateFormat;
 
 public class Persona {
 	private String Nome,Cognome,CF;
-	private String DataNascita;
+	private Date DataNascita;
 	
-	public Persona(String Nome, String Cognome, String CF, String DataNascita) {
+	public Persona(String Nome, String Cognome, String CF, Date DataNascita) {
 		this.Nome = Nome;
 		this.Cognome = Cognome;
 		this.CF = CF;
 		this.DataNascita = DataNascita;
 	}
 
-	public String getCF() {
-		return CF;
-	}
 
 	
-
+	
+	
 
 }
