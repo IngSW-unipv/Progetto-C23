84bb71d293a419f6da58f886de202eaad8e50c9f
Stefany <stefanymaria.vecchio01@universitadipavia.it>
2023-02-08 19:51:37 +0100

    Lavoro insieme sulle iscrizioni e date
    Co-Authored-By: DeniseVaccarella <119804550+DeniseVaccarella@users.noreply.github.com>
    Co-Authored-By: Rebecca Maccagni <119804491+RebeccaMaccagni@users.noreply.github.com>
    Co-Authored-By: VironicaEissa <120571165+VironicaEissa@users.noreply.github.com>

--- FitUP/src/it/unipv/sfw/esame/progettoC23/GestioneIscrizione.java
+++ FitUP/src/it/unipv/sfw/esame/progettoC23/GestioneIscrizione.java
@@ -1,34 +1,64 @@
 package it.unipv.sfw.esame.progettoC23;
-import java.util.Date;
+import java.util.*;
+
 import java.text.DateFormat; // mettere formato data short!!
 
-public  class GestioneIscrizione {
-	protected GestioneIscrizione CodiceBadge;
+public class GestioneIscrizione {
+	private String CodiceBadge; 
 	private String Rinnovo;
 	protected Persona Iscritto;
-	protected Date PrimaIscrizione,DataRinnovo;
+	protected Calendar PrimaIscrizione;
+	protected Calendar DataRinnovo;
 	protected TipoAbbonamento Abbonamento; 
+	private final String CodicePalestra = "FitUp";
 	
-// DATARINNOVO è dinamica, va calcolata aggiungendo il tipo di abbonamento alla  prima iscrizione 
-// ad esempio mi iscrivo il 01/01/2023 e scelgo semestrale, DataRinnovo sarà 01/06/2023 
-//attenzione se non è il primo rinnovo sommo il valore del tipoabbonamento alla datarinnovo che poi si aggiorna
-	public GestioneIscrizione( Persona iscritto, Date primaIscrizione, TipoAbbonamento abbonamento) {
-		this.Iscritto = iscritto;
-		this.PrimaIscrizione = primaIscrizione;
-		this.Abbonamento = abbonamento;
+	
+	public GestioneIscrizione(Persona iscritto,TipoAbbonamento abbonamento) 
+	{
+	this.Iscritto = iscritto;
+	this.PrimaIscrizione =  Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"), Locale.ITALY);
+	this.Abbonamento = abbonamento; 
+	
+	}
+	
+	
+	public Calendar getPrimaIscrizione () {
+		return PrimaIscrizione;
 	}
 
-	public GestioneIscrizione() {
-		// TODO Auto-generated constructor stub
+	public void setCodiceBadge(Persona iscritto) {
+		CodiceBadge = iscritto.getCF() + CodicePalestra ;
 	}
 
-	/* public String getCodiceBadge() {
+
+	public String getCodiceBadge() {
 		return CodiceBadge;
 	}
-
-	public void setCodiceBadge(String codiceBadge) {
-		CodiceBadge = codiceBadge;
+	
+	public void setDataRinnovo(TipoAbbonamento Abbonamento) {
+		
+	this.Abbonamento = Abbonamento;
+	//this.PrimaIscrizione = PrimaIscrizione;
+		
+		
+		switch (Abbonamento) {
+		case MENSILE:	
+			PrimaIscrizione.add(Calendar.MONTH, +1);
+			break;
+		case SEMESTRALE:
+			PrimaIscrizione.add(Calendar.MONTH, +6);
+			break;
+		case ANNUALE:
+			PrimaIscrizione.add(Calendar.YEAR, +1);
+			break;
+			
+		}
 	}
-	*/
-
+	
+	public Calendar getDataRinnovo() {
+		return PrimaIscrizione;
+	}
+	
+	
+	
 }
--- FitUP/src/it/unipv/sfw/esame/progettoC23/Persona.java
+++ FitUP/src/it/unipv/sfw/esame/progettoC23/Persona.java
@@ -1,22 +1,21 @@
 package it.unipv.sfw.esame.progettoC23;
 
-import java.util.Date;
 import java.text.DateFormat;
 
 public class Persona {
-	private String Nome,Cognome,CF;
-	private Date DataNascita;
+	private String Nome,Cognome,CF, DataNascita;
 	
-	public Persona(String Nome, String Cognome, String CF, Date DataNascita) {
+	public Persona(String Nome, String Cognome, String CF, String DataNascita) {
 		this.Nome = Nome;
 		this.Cognome = Cognome;
 		this.CF = CF;
 		this.DataNascita = DataNascita;
 	}
 
+	public String getCF() {
+		return CF;
+	}
 
-	
-	
 	
 
 }
