354dce37a197e5135065871ab2b8e55b9ac3faef
Rebecca Maccagni <119804491+RebeccaMaccagni@users.noreply.github.com>
2023-02-08 19:22:05 +0100

    Proseguimento
    Co-Authored-By: DeniseVaccarella <119804550+DeniseVaccarella@users.noreply.github.com>
    Co-Authored-By: Stefany <stefanymaria.vecchio01@universitadipavia.it>
    Co-Authored-By: VironicaEissa <120571165+VironicaEissa@users.noreply.github.com>

---
+++ FitUP/src/it/unipv/sfw/esame/progettoC23/src/it/unipv/sfw/esame/progettoC23/GestioneIscrizione.java
@@ -0,0 +1,69 @@
+package it.unipv.sfw.esame.progettoC23;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
+import java.util.TimeZone;
+import java.text.DateFormat; // mettere formato data short!!
+
+public class GestioneIscrizione {
+	private String CodiceBadge; 
+	private String Rinnovo;
+	protected Persona Iscritto;
+	protected Calendar PrimaIscrizione;
+	protected Calendar DataRinnovo;
+	protected TipoAbbonamento Abbonamento; 
+	private final String CodicePalestra = "FitUp";
+	
+	
+	public GestioneIscrizione(Persona iscritto,TipoAbbonamento abbonamento) 
+	{
+	this.Iscritto = iscritto;
+	this.PrimaIscrizione =  Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"), Locale.ITALY);
+	this.Abbonamento = abbonamento; 
+	
+	}
+	
+	
+	public Calendar getPrimaIscrizione () {
+		return PrimaIscrizione;
+	}
+
+	public void setCodiceBadge(Persona iscritto) {
+		CodiceBadge = iscritto.getCF() + CodicePalestra ;
+	}
+
+
+	public String getCodiceBadge() {
+		return CodiceBadge;
+	}
+	
+	public void setDataRinnovo(TipoAbbonamento Abbonamento) {
+		
+	this.Abbonamento = Abbonamento;
+	//this.PrimaIscrizione = PrimaIscrizione;
+		
+		
+		switch (Abbonamento) {
+		case MENSILE:	
+			PrimaIscrizione.add(Calendar.MONTH, +1);
+			break;
+		case SEMESTRALE:
+			PrimaIscrizione.add(Calendar.MONTH, +6);
+			break;
+		case ANNUALE:
+			PrimaIscrizione.add(Calendar.YEAR, +1);
+			break;
+			
+		}
+	}
+	
+	public Calendar getDataRinnovo() {
+		return PrimaIscrizione;
+	}
+	
+	
+	
+}	
+	
+
+
--- FitUP/src/it/unipv/sfw/esame/progettoC23/src/it/unipv/sfw/esame/progettoC23/Persona.java
+++ FitUP/src/it/unipv/sfw/esame/progettoC23/src/it/unipv/sfw/esame/progettoC23/Persona.java
@@ -5,18 +5,20 @@ import java.text.DateFormat;
 
 public class Persona {
 	private String Nome,Cognome,CF;
-	private Date DataNascita;
+	private String DataNascita;
 	
-	public Persona(String Nome, String Cognome, String CF, Date DataNascita) {
+	public Persona(String Nome, String Cognome, String CF, String DataNascita) {
 		this.Nome = Nome;
 		this.Cognome = Cognome;
 		this.CF = CF;
 		this.DataNascita = DataNascita;
 	}
 
+	public String getCF() {
+		return CF;
+	}
 
 	
-	
-	
+
 
 }
