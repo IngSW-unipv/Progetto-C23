ef222f2dc2b678ab472a85e01fcecf20bfe8e5fb
Rebecca Maccagni <119804491+RebeccaMaccagni@users.noreply.github.com>
2023-02-16 19:33:03 +0100

    Update Corso.java
    Co-Authored-By: DeniseVaccarella <119804550+DeniseVaccarella@users.noreply.github.com>

--- FitUP/src/it/unipv/sfw/esame/progettoC23/Corso.java
+++ FitUP/src/it/unipv/sfw/esame/progettoC23/Corso.java
@@ -3,15 +3,25 @@ package it.unipv.sfw.esame.progettoC23;
 public class Corso {
 	private TipoCorso NomeCorso;
 	private static final int MaxIscritti=30;
+	
+	protected Corso NomeCorso;
+	private static final int MaxIscritti = 30;
 	private int N_Iscritti;
 	private Persona Istruttore;
 	
 	public Corso(TipoCorso NomeCorso, Persona Istruttore) {
+	public Corso (Corso NomeCorso, Persona Istruttore, int N_iscritti) {
+		
 		this.NomeCorso = NomeCorso;
 		this.N_Iscritti = 0;
+		this.N_Iscritti = N_iscritti = 0;
 		this.Istruttore = Istruttore;
+		
 	}
 	
 	
 
+	/* public int getN_Iscritti() {
+		return N_Iscritti;
+	} */
 }
