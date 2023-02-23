a869cbf4eae05370c1dbbb967f040b5e827e16e6
Rebecca Maccagni <119804491+RebeccaMaccagni@users.noreply.github.com>
2023-02-24 00:11:26 +0100

    Update IscrittoContoller.java

--- FitUP/src/it/unipv/sfw/esame/progettoC23/controller/IscrittoContoller.java
+++ FitUP/src/it/unipv/sfw/esame/progettoC23/controller/IscrittoContoller.java
@@ -2,11 +2,16 @@ package it.unipv.sfw.esame.progettoC23.controller;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.Locale;
+import java.util.TimeZone;
 
 import javax.swing.JOptionPane;
 
 import it.unipv.sfw.esame.progettoC23.Badge;
 import it.unipv.sfw.esame.progettoC23.Rinnovo;
+import it.unipv.sfw.esame.progettoC23.TipoAbbonamento;
+import it.unipv.sfw.esame.progettoC23.jdbc.bean.Iscrizione;
 import it.unipv.sfw.esame.progettoC23.view.IscrittoView;
 
 public class IscrittoContoller {
@@ -14,6 +19,7 @@ public class IscrittoContoller {
 	private final Rinnovo rinnovo;
 	private final IscrittoView view;
 	private final Badge badge;
+	private Calendar PrimaIscrizione = Calendar.getInstance(TimeZone.getTimeZone("Europe/Rome"), Locale.ITALY);
 	
 	public IscrittoContoller (Rinnovo rinnovo, IscrittoView view, Badge badge) {
 		
@@ -26,15 +32,29 @@ public class IscrittoContoller {
 	
 	public void setListeners() {
 		
+			
 		view.getVedi().addActionListener(new ActionListener() {
 			
 			@Override
 			public void actionPerformed (ActionEvent e) {
-				
-				
-			//JOptionPane.showInputDialog(rinnovo.getDataRinnovo().getTime(), "Ecco la data del tuo rinnovo!")	;
 			
-			view.setRinnovo(rinnovo.getDataRinnovo());
+			
+			switch (view.getRinnovo()) {
+			case "MENSILE":	
+			PrimaIscrizione.add(Calendar.MONTH, +1);
+			view.setRinnovo(PrimaIscrizione.add(Calendar.MONTH, +1));
+			break;
+			case "SEMESTRALE":
+			PrimaIscrizione.add(Calendar.MONTH, +6);
+			view.setRinnovo(PrimaIscrizione.add(Calendar.MONTH, +6));
+			break;
+			case "ANNUALE":
+			PrimaIscrizione.add(Calendar.YEAR, +1);
+			view.setRinnovo(PrimaIscrizione.add(Calendar.MONTH, +12));
+			break;
+						
+				}
+			
 			
 					
 				
@@ -47,11 +67,13 @@ public class IscrittoContoller {
 			public void actionPerformed (ActionEvent e) {
 				
 				
-				view.setBadge(badge.getCodiceBadge());
+			view.setBadge(view.getCf());
 					
 			}
 			
 		});
+		
+		
 		
 		
 	}
