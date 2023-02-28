35381b03fafa5cb0f7729eb223921c43da6d4c8e
Stefany <stefanymaria.vecchio01@universitadipavia.it>
2023-02-28 17:59:11 +0100

    lavoro su view e controller pt.1

--- FitUP/src/it/unipv/sfw/esame/progettoC23/controller/VisitaController.java
+++ FitUP/src/it/unipv/sfw/esame/progettoC23/controller/VisitaController.java
@@ -1,7 +1,23 @@
 package it.unipv.sfw.esame.progettoC23.controller;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import it.unipv.sfw.esame.progettoC23.view.IscrittoView;
+import it.unipv.sfw.esame.progettoC23.view.VisitaView;
 
 public class VisitaController {
+	private final VisitaView viewvisita;
+
+	public VisitaController(VisitaView viewvisita) {
+		this.viewvisita = viewvisita;
+		setListeners();
+	}
+	
+public void setListeners() {
+		
+		
+		}
 	
 	
 
